fafa14997cf73c9cda7fa5cd3d6ab4b2020c609d
lntoTheRain <lntotherain@qq.com>
2016-10-27 15:59:50 +0800

    chushihua project

---
+++ huacaizhibo/huacaizhibo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  huacaizhibo
+//
+//  Created by mac on 2016/10/27.
+//  Copyright © 2016年 julia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        
+    }
+
+
+}
+
